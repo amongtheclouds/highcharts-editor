54f5e14b7d3df1b1253639e2e0d3e316226f0a65
Ian Ogilvie <ian@gmri.org>
2018-02-16 15:28:25 -0500

    The "files" array of files for the browser to load was being appended to each time creating duplicates for every site access.

--- src/ui/highed.dataimporter.js
+++ src/ui/highed.dataimporter.js
@@ -93,7 +93,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
             properties = highed.merge({
                 options: ['csv', 'plugins', 'samples'],
                 plugins: ['CSV', 'JSON', 'Difi', 'Socrata', 'Google Spreadsheets',
-                     'ERDDAP', 'Climatologies', 'Anomalies', 'LoopbackTest1']
+                     'ERDDAP', 'Climatologies', 'Anomalies', 'LoopbackTest1', 'LoopbackClimatologies']
             }, attributes),
 
             tabs = highed.TabControl(parent, false, true),
